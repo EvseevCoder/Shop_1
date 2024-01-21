f81ce9c95f4823f6eee30e5960197397e7b9e277
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2024-01-21 07:59:36 +0300

    (21.01.24)

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,34 +1,63 @@
 class Products {
+  constructor() {
+    this.classNameActive = "productsElement__btn_active";
+    this.labelAdd = "Добавить в корзину";
+    this.labelRemove = "Удалить из корзины";
+  }
 
-    render() {
+  handleSetLocationStorage(element, id) {
+    const { pushProduct, products } = locatStorageUtil.putProducts(id);
 
-        let htmlCatalog = ''
+    console.log(pushProduct);
 
-        CATALOG.forEach( ({id, name, price, img}) => {
-            htmlCatalog += `
+    if (pushProduct) {
+      element.classList.add(this.classNameActive);
+      element.innerHTML = this.labelRemove;
+      console.log("Удалено");
+    } else {
+      element.classList.remove(this.classNameActive);
+      element.innerHTML = this.labelAdd;
+      console.log("Добавлено");
+    }
+  }
+
+  render() {
+    const productsStore = locatStorageUtil.getProducts();
+    let htmlCatalog = "";
+
+    CATALOG.forEach(({ id, name, price, img }) => {
+      let activeClass = "";
+      let activetext = "";
+
+      if (productsStore.indexOf(id) == -1) {
+        activetext = this.labelAdd;
+      } else {
+        activeClass = " " + this.classNameActive;
+        activetext = this.labelRemove;
+      }
+
+      htmlCatalog += `
             
-                <li class="productsElement">
-                    <span class="productsElement__name">${name}</span>
-                    <img class="productsElement__img" src="${img}"/>
-                    <span class="productsElement__price">⚡ ${price.toLocaleString()} RUB</span>
-                    <button class="productsElement__btn">Добавить в корзину</button>
-                </li>
+        <li class="productsElement">
+            <span class="productsElement__name">${name}</span>
+            <img class="productsElement__img" src="${img}"/>
+            <span class="productsElement__price">⚡ ${price.toLocaleString()} RUB</span>
+            <button class="productsElement__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}')">${activetext}</button>
+        </li>
 
-            `
-        });
+      `;
+    });
 
-
-        const html = `
+    const html = `
         
             <ul class='productsContainer'>${htmlCatalog}</ul>
 
-        `
+        `;
 
-        ROOT_PRODUCTS.innerHTML = html
-
-    }
+    ROOT_PRODUCTS.innerHTML = html;
+  }
 }
 
-productsPage = new Products()
+productsPage = new Products();
 
-productsPage.render()
+productsPage.render();
--- utils/locatStorageUtil.js
+++ utils/locatStorageUtil.js
@@ -1,46 +1,35 @@
 class LocatStorageUtil {
-    constructor() {
-        this.keyName = 'products'
+  constructor() {
+    this.keyName = "products";
+  }
+
+  getProducts() {
+    const productsLovalStorage = localStorage.getItem(this.keyName);
+    if (productsLovalStorage != null) {
+      return JSON.parse(productsLovalStorage);
+    }
+  }
+
+  putProducts(id) {
+    let products = this.getProducts();
+    const index = products.indexOf(id);
+    let pushProduct = false;
+
+    if (index == -1) {
+      products.push(id);
+      this.pushProduct = true;
+    } else {
+      products.splice(index, 1);
     }
 
-    getProducts() {
-        const productsLovalStorage = localStorage.getItem(this.keyName)
-        if (productsLovalStorage != null) {
-            return JSON.parse(productsLovalStorage)
-        }
+    localStorage.setItem(this.keyName, JSON.stringify(products));
 
-        
-    }
-
-    putProducts(id) {
-        let products = this.getProducts()
-        const index = products.indexOf(id)
-        let pushProduct = false
-
-        if (index == -1) {
-            products.push(id)
-            this.putProducts = true
-        } else {
-            products.splice(index, 1)
-        }
-
-        products.push(id)
-        localStorage.setItem(this.keyName, JSON.stringify(products))
-
-        return {
-            pushProduct,
-            products
-        }
-    }
+    return {
+      pushProduct,
+      products,
+    };
+  }
 }
 
 const locatStorageUtil = new LocatStorageUtil();
-let a = locatStorageUtil.getProducts()
-
-console.log(a)
-
-locatStorageUtil.putProducts('el3')
-locatStorageUtil.putProducts('el4')
-
-
-console.log(a)
+let a = locatStorageUtil.getProducts();
